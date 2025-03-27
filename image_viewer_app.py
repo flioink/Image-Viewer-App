3854a5c528fcb28d3e37e72b568b0f6f407bda43
flioink <phantom_warrior@abv.bg>
2025-03-27 03:34:58 +0200

    add scrolling

--- image_viewer_app.py
+++ image_viewer_app.py
@@ -1,8 +1,9 @@
 import json
 import os
-
+from PIL import ImageFilter, Image, ImageOps
+from PIL.ImageQt import ImageQt
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, \
     QListWidget, QLabel, QListWidgetItem, QFileDialog
 
@@ -19,6 +20,8 @@ class ImageViewer(QWidget):
         self.folder_path = None
         self.settings_file = "settings.json"
         self.scaled_max_dimension = 800
+        self.current_filepath = ""
+        self.current_index = 0
 
         self.init_UI()
         self.event_handler()
@@ -35,10 +38,23 @@ class ImageViewer(QWidget):
         self.file_layout.addWidget(self.open_folder_button)
 
         # image layout
-        self.image_layout = QHBoxLayout()
+        self.image_layout = QVBoxLayout()
         self.image_label = QLabel()
         self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # image fileter buttons
+        self.filter_buttons_layout = QHBoxLayout()
+        self.contour_button = QPushButton("Contour")
+        self.blur_button = QPushButton("Blur")
+        self.invert_button = QPushButton("Invert")
+        self.gray_button = QPushButton("Grayscale")
+        self.filter_buttons_layout.addWidget(self.contour_button)
+        self.filter_buttons_layout.addWidget(self.blur_button)
+        self.filter_buttons_layout.addWidget(self.invert_button)
+        self.filter_buttons_layout.addWidget(self.gray_button)
+        #
         self.image_layout.addWidget(self.image_label)
+        self.image_layout.addLayout(self.filter_buttons_layout)
 
         self.master_layout.addLayout(self.file_layout, 1)
         self.master_layout.addLayout(self.image_layout, 3)
@@ -49,6 +65,10 @@ class ImageViewer(QWidget):
     def event_handler(self):
         self.open_folder_button.clicked.connect(self.set_directory)
         self.file_list.itemClicked.connect(self.display_image)
+        self.contour_button.clicked.connect(self.contour)
+        self.blur_button.clicked.connect(self.blur)
+        self.invert_button.clicked.connect(self.invert)
+        self.gray_button.clicked.connect(self.grayscale)
 
     def set_directory(self):
         path = QFileDialog.getExistingDirectory()
@@ -59,23 +79,23 @@ class ImageViewer(QWidget):
 
     def display_image(self, item):
         # Get full file path
-        filepath = os.path.join(self.folder_path, item.text())
+        self.current_filepath = os.path.join(self.folder_path, item.text())
+
+        # Set the index of the current item here on click
+        self.current_index = self.file_list.row(item)
+
         # Create QPixmap object
-        pixmap = QPixmap(filepath).scaled(
-            self.scaled_max_dimension,
-            self.scaled_max_dimension,
-            Qt.AspectRatioMode.KeepAspectRatio,
-            Qt.TransformationMode.SmoothTransformation
-            )
+        pixmap = self.create_pixmap_from_url(self.current_filepath)
         # Set the QPixmap object on the label to display the image
         self.image_label.setPixmap(pixmap)
+
 
     def load_images(self, folder_path):
         self.file_list.clear()  # Clear any existing items
 
         for filename in os.listdir(folder_path):
             # Check valid image files
-            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            if filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif", ".webp")):
                 item = QListWidgetItem(filename)
                 self.file_list.addItem(item)
 
@@ -87,9 +107,9 @@ class ImageViewer(QWidget):
             first_filepath = os.path.join(folder_path, first_filename)
 
             # Load and display the first image
-            pixmap = QPixmap(first_filepath).scaled(self.scaled_max_dimension,
-                                                    self.scaled_max_dimension,
-                                                    Qt.AspectRatioMode.KeepAspectRatio)
+            pixmap = self.create_pixmap_from_url(first_filepath)
+            self.current_filepath = first_filepath
+
             self.image_label.setPixmap(pixmap)
         else:
             print("File not found!")
@@ -113,6 +133,153 @@ class ImageViewer(QWidget):
         source_path = os.path.normpath(self.folder_path)
         with open(self.settings_file, "w") as file:
             json.dump({"source": source_path}, file)
+
+
+
+
+
+    def wheelEvent(self, event):
+        delta = event.angleDelta().y()
+        if delta > 0:  # Scroll up
+            self.current_index = (self.current_index - 1) % len(self.file_list)
+        else:  # Scroll down
+            self.current_index = (self.current_index + 1) % len(self.file_list)
+
+        self.load_image(self.current_index)
+        #print(self.current_index)
+
+    def load_image(self, index):
+        if 0 <= index < len(self.file_list):
+            item = self.file_list.item(index)
+
+            if item:
+                item_filename = item.text()
+                item_filepath = os.path.join(self.folder_path, item_filename)
+
+                # Load and display the current image
+                pixmap = self.create_pixmap_from_url(item_filepath)
+                self.current_filepath = item_filepath
+
+                self.image_label.setPixmap(pixmap)
+
+            print(index)
+
+    def create_pixmap_from_url(self, image):
+        """Creates a pixmap object to be displayed"""
+        # Load and display the current image
+        pixmap = QPixmap(image).scaled(
+            self.scaled_max_dimension,
+            self.scaled_max_dimension,
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+            )
+
+        return pixmap
+
+
+    def create_pixmap_from_image(self, image):
+        """Creates a pixmap object to be displayed"""
+        # Load and display the current image
+        pixmap = QPixmap.fromImage(image).scaled(
+            self.scaled_max_dimension,
+            self.scaled_max_dimension,
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+            )
+
+        return pixmap
+
+    def contour(self):
+
+        image = Image.open(self.current_filepath)
+        print(f"Image Mode: {image.mode}")
+
+        image = image.filter(ImageFilter.CONTOUR)
+        #image.show()
+        # Convert to RGB if the image has an alpha channel
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+
+        try:
+
+            q_image = ImageQt(image)
+
+            pixmap = self.create_pixmap_from_image(q_image)
+
+            self.image_label.setPixmap(pixmap)
+
+        except IOError as e:
+            print(f"Error processing image {self.current_filepath}: {e}")
+
+    def blur(self):
+
+        image = Image.open(self.current_filepath)
+        print(f"Image Mode: {image.mode}")
+
+        image = image.filter(ImageFilter.GaussianBlur(radius=9))
+
+        #image.show()
+        # Convert to RGB if the image has an alpha channel
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+
+        try:
+
+            q_image = ImageQt(image)
+
+            pixmap = self.create_pixmap_from_image(q_image)
+
+            self.image_label.setPixmap(pixmap)
+
+        except IOError as e:
+            print(f"Error processing image {self.current_filepath}: {e}")
+
+    def invert(self):
+
+        image = Image.open(self.current_filepath)
+        print(f"Image Mode: {image.mode}")
+
+        image = ImageOps.invert(image.convert('RGB'))
+
+        #image.show()
+        # Convert to RGB if the image has an alpha channel
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+
+        try:
+
+            q_image = ImageQt(image)
+
+            pixmap = self.create_pixmap_from_image(q_image)
+
+            self.image_label.setPixmap(pixmap)
+
+        except IOError as e:
+            print(f"Error processing image {self.current_filepath}: {e}")
+
+    def grayscale(self):
+
+        image = Image.open(self.current_filepath)
+        print(f"Image Mode: {image.mode}")
+
+        image = image.convert('L')
+
+        # image.show()
+        # Convert to RGB if the image has an alpha channel
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+
+        try:
+
+            q_image = ImageQt(image)
+
+            pixmap = self.create_pixmap_from_image(q_image)
+
+            self.image_label.setPixmap(pixmap)
+
+        except IOError as e:
+            print(f"Error processing image {self.current_filepath}: {e}")
+
 
 
 
